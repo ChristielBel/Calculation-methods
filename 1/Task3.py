925b54553981fb3af8544c75ae510058286ac64e
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2024-09-09 21:51:47 +0300

    Update Task3.py

--- 1/Task3.py
+++ 1/Task3.py
@@ -1,6 +1,5 @@
 import math
 
-# Полный ряд для больших x
 def s_full(x, tolerance=8e-3):
     sum_s = 0.0
     k = 1
@@ -14,20 +13,14 @@ def s_full(x, tolerance=8e-3):
         k += 1
     return sum_s
 
-# Быстрое приближение для малых x
 def s_approx(x):
     zeta_4 = math.pi**4 / 90
     return x * zeta_4
 
-# Основная функция
-def s_x(x):
-    if abs(x) < 0.1:
-        return s_approx(x)
-    else:
-        return s_full(x)
-
 # Вычисление для x = 0.5 и x = 0.999999999
-x1 = 0.5
-x2 = 0.999999999
-print(f"s({x1}) = {s_x(x1)}")
-print(f"s({x2}) = {s_x(x2)}")
+x_values = [0.5, 0.999999999]
+for x in x_values:
+    result_exact = s_full(x)
+    result_fast = s_approx(x)
+    print(f"x = {x}, s(x) = {result_exact:.8f}")
+    print(f"x = {x}, s(x) (быстрый метод) = {result_fast:.8f}")
